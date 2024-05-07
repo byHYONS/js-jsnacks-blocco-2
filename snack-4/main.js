cf1d7e7459b0f1ddb015403ebcc99618454c6731
byHYONS <byhyons@gmail.com>
2024-05-07 10:06:18 +0200

    [UPD] done snack-4

--- snack-4/main.js
+++ snack-4/main.js
@@ -5,3 +5,15 @@
 // Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
 
+const elementoUno = [1, 2, 3, 4, 5];
+const elementoDue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log({elementoUno});
+console.log({elementoDue});
+
+while (elementoUno.length < elementoDue.length) {
+    elementoUno.push(Math.floor(Math.random() * 100));
+}
+
+console.log({elementoUno});
+console.log({elementoDue});
