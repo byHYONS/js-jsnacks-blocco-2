7f606ffc24b7f5b79c88a6c2e248fb1ade2555bc
byHYONS <byhyons@gmail.com>
2024-05-07 10:00:40 +0200

    first commit

---
+++ snack-4/main.js
@@ -0,0 +1,7 @@
+// reset
+'use strict'
+
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
+
