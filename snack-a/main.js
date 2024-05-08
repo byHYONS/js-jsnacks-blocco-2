484ff2f5f58e773d27756e4bbd4cb41307576ed5
byHYONS <byhyons@gmail.com>
2024-05-08 18:02:44 +0200

    [FIT]

--- snack-a/main.js
+++ snack-a/main.js
@@ -5,7 +5,6 @@
 // arrey in input:
 const listaElementi = [1, 2, 3, 4, 5, 'pane', 'pasta', 'birra', true, false];
 
-
 // funzione per calcolare il numero degli elementi:
 function contaElementi(nomeArrey){
     let counter = 0
@@ -21,7 +20,6 @@ function contaElementi(nomeArrey){
 // salvo il risultato in var e stampo:
 const risultato = contaElementi(listaElementi);
 console.log({risultato});
-
 
 // FINE
 
